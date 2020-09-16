c6d38f2df21ce3c2ab7bc9e8d19901e4738baf95
arunkarthik132 <karthikarun172@gmail.com>
2020-09-16 08:13:38 +0530

    12th

--- GeneralPrac.cpp
+++ GeneralPrac.cpp
@@ -1,46 +1,33 @@
-// C++ program to demonstrate insertion
-// into a vector of vectors
-
-#include <iostream>
+#include <cstdio>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+// Use of comparator
+//comparing function only sorts if string size is equal and keeps the larger integgers at last.
+bool myfunction (string i,string j) 
+{ 
+    int n=i.length();
+    int m=j.length();
+    if(n==m)
+        return (i<j);
+    
+    return n<m;   
+}
 
-// Defining the rows and columns of
-// vector of vectors
-#define ROW 4
-#define COL 5
 
-int main()  
-{
-    // Initializing the vector of vectors
-    vector<vector<int> > vec;
-
-    // Elements to insert in column
-    int num = 10;
-
-    // Inserting elements into vector
-    for (int i = 0; i < ROW; i++)
-    {
-        // Vector to store column elements
-        vector<int> v1;
-
-        for (int j = 0; j < COL; j++)
-        {
-            v1.push_back(num);
-            num += 5;
-        }
-
-        // Pushing back above 1D vector
-        // to create the 2D vector
-        vec.push_back(v1);
-    }
-
-    // Displaying the 2D vector
-    for (int i = 0; i < vec.size(); i++)
-    {
-        for (int j = 0; j < vec[i].size(); j++)
-            cout << vec[i][j] << " ";
-        cout << endl;
-    }
+int main() {
+    int n;
+    cin>>n;
+    vector <string> arr(n);
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    
+    
+    sort(arr.begin(),arr.end(),myfunction);
+    
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<endl;
+    
     return 0;
 }
--- STL_Prac.cpp
+++ STL_Prac.cpp
@@ -1,12 +1,46 @@
+// C++ program to demonstrate insertion
+// into a vector of vectors
+
 #include <iostream>
 #include <vector>
-#include <list>
-#include <algorithm>
-
 using namespace std;
 
-int main()
+// Defining the rows and columns of
+// vector of vectors
+#define ROW 4
+#define COL 5
+
+int main()  
 {
-    vector<int> a = {1, 23, 4, 54, 7};
-    cout << a[1] << endl;
+    // Initializing the vector of vectors
+    vector<vector<int> > vec;
+
+    // Elements to insert in column
+    int num = 10;
+
+    // Inserting elements into vector
+    for (int i = 0; i < ROW; i++)
+    {
+        // Vector to store column elements
+        vector<int> v1;
+
+        for (int j = 0; j < COL; j++)
+        {
+            v1.push_back(num);
+            num += 5;
+        }
+
+        // Pushing back above 1D vector
+        // to create the 2D vector
+        vec.push_back(v1);
+    }
+
+    // Displaying the 2D vector
+    for (int i = 0; i < vec.size(); i++)
+    {
+        for (int j = 0; j < vec[i].size(); j++)
+            cout << vec[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
 }
